3658d23b7d0015041a899f1fbdfe622ec615732a
viniciusrs1 <contatoviniciusrs@gmail.com>
2023-06-16 16:25:12 -0300

    feat: navbar e botao de logout

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,10 +3,16 @@ import { CommonModule } from '@angular/common';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { AngularMaterialModule } from './modules/angular-material.module';
 import { CommonModulesModule } from './modules/common-modules.module';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
-  declarations: [SidebarComponent],
+  declarations: [SidebarComponent, NavbarComponent],
   imports: [CommonModule, AngularMaterialModule, CommonModulesModule],
-  exports: [SidebarComponent, AngularMaterialModule, CommonModulesModule],
+  exports: [
+    SidebarComponent,
+    NavbarComponent,
+    AngularMaterialModule,
+    CommonModulesModule,
+  ],
 })
 export class SharedModule {}
